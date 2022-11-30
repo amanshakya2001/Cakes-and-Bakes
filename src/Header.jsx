866a28660a1b71157cc39eb4c6260c5291c27659
Aman Shakya <aman.shakya@analyticsvidhya.com>
2022-11-30 13:32:39 +0530

    gh-pages build

--- src/Header.jsx
+++ src/Header.jsx
@@ -9,7 +9,7 @@ export default function Header(props) {
     <>
         <header>
         <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
-            <a className="navbar-brand" href="/"><img src={"./assests/images/headerLogo.png"} alt="" width={'200px'} /></a>
+            <a className="navbar-brand" href="/CakesandBakes/"><img src={"./assests/images/headerLogo.png"} alt="" width={'200px'} /></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
